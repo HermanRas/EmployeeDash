64be59d3459e7c1ff99380d0999a84d50e1ce41f
Herman Ras <hermanr@PetraGroup.local>
2020-02-28 11:22:37 +0200

    Renamed The Report

--- getCSV.php
+++ getCSV.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=\"my-data.csv\"");
+header("Content-Disposition: attachment; filename=\"WebReport.csv\"");
 $data=stripcslashes($_REQUEST['csv_text']);
 echo $data; 
 ?>
