64f5371975a9e1a73a77670e6e0e01a8a902e59d
Herman Ras <hermanr@PetraGroup.local>
2020-04-08 09:14:16 +0200

    updated Local Dir

--- js/browserCheck.js
+++ js/browserCheck.js
@@ -4,6 +4,6 @@ var msieOld = ua.indexOf("MSIE");
 
 if ((msie > 0) || (msieOld > 0)) // If Internet Explorer, return version number
 {
-    let response = '<h1>Please use a modern browser</h1><br><img alt="Please update your browser" width="650px;" src="https://dat-ser-web-01.petragroup.local/EmployeeStatus/img/ModernBrowser.png" border="0">'
+    let response = '<h1>Please use a modern browser</h1><br><img alt="Please update your browser" width="650px;" src="img/ModernBrowser.png" border="0">'
     document.getElementById("Loader").innerHTML = response;
 }
