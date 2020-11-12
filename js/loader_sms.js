b173f7b04232d3c6be5525c568a50b204ebaa855
Herman Ras <hermanr@PetraGroup.local>
2020-11-12 11:51:42 +0200

    Added SMS Dash

---
+++ js/loader_sms.js
@@ -0,0 +1,36 @@
+//global vars.
+console.log('Fetching SMS Data');
+
+function fixTable() {
+	var table = $('#example').DataTable({
+		"scrollX": true
+	});
+
+	$('a.toggle-vis').on('click', function (e) {
+		e.preventDefault();
+
+		// Get the column API object
+		var column = table.column($(this).attr('data-column'));
+
+		// Toggle the visibility
+		column.visible(!column.visible());
+	});
+};
+
+function getCSVData() {
+	var csv_value = $('#example').table2CSV({
+		delivery: 'value'
+	});
+	$("#csv_text").val(csv_value);
+}
+
+let search = window.location.search;
+
+fetch(sms_url + search)
+	.then(response => response.text())
+	.then((response) => {
+		//console.log(response)
+		document.getElementById("Loader").innerHTML = response;
+		fixTable();
+	})
+	.catch(err => console.log(err))
